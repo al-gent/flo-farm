9ca904b7736702ead1fd0b0049ee749136fc5f80
al-gent <94gent@gmail.com>
2026-02-26 11:47:53 -0800

    wipe

--- components/product-multiplier.js
+++
@@ -1,6 +0,0 @@
-export default function productMultiplier(product) {
-  const productMultiplier = product.unitratio
-    ? product.unitratio
-    : (product.price[0] / product.price[1]).toFixed(2);
-  return parseFloat(productMultiplier);
-}
--- pages/api/get-orders.js
+++
@@ -1,9 +0,0 @@
-import { sql } from '@vercel/postgres';
-
-export default async function getOrders(req, res) {
-  const { client } = req.query;
-  const orders = (
-    await sql`SELECT * FROM Orders WHERE status != 'completed' AND client = ${client};`
-  ).rows;
-  res.status(200).json(orders);
-}
